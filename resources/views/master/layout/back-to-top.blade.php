5c5b7645519d2623997c3c6669607798608fe614
Kenan Gündoğan <19374764+kenangundogan@users.noreply.github.com>
2024-06-06 11:31:01 +0300

    Add back-to-top button to master layout

---
+++ resources/views/master/layout/back-to-top.blade.php
@@ -0,0 +1,3 @@
+<div onclick="topFunction()" id="backToTopBtn" class="fixed z-50 right-10 bottom-10 w-12 h-12 shadow-xl bg-white flex justify-center items-center hidden cursor-pointer hover:bg-gray-100">
+    <i class="fa-solid fa-chevron-up"></i>
+</div>
