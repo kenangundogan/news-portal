80eefcfdb0d994643b672cd85e73651071cc804e
Kenan Gündoğan <19374764+kenangundogan@users.noreply.github.com>
2024-06-14 12:35:52 +0300

    feat: Update CMS menu links styling
    - Add "!p-2 text-sm" class to CMS menu links for consistent styling
    - This change improves the visual appearance of the menu links in the CMS layout

--- resources/views/cms/master/layout/menu.blade.php
+++ resources/views/cms/master/layout/menu.blade.php
@@ -1,6 +1,6 @@
 <div class="relative px-4 md:px-12 py-2">
-    <x-cms.base.link href="{{ route('images.index') }}" content="{{ __('Images') }}"/>
-    <x-cms.base.link href="{{ route('categories.index') }}" content="{{ __('Categories') }}"/>
-    <x-cms.base.link href="{{ route('news.index') }}" content="{{ __('News') }}"/>
-    <x-cms.base.link href="{{ route('users.index') }}" content="{{ __('Users') }}"/>
+    <x-cms.base.link class="!p-2 text-sm" href="{{ route('images.index') }}" content="{{ __('Images') }}"/>
+    <x-cms.base.link class="!p-2 text-sm" href="{{ route('categories.index') }}" content="{{ __('Categories') }}"/>
+    <x-cms.base.link class="!p-2 text-sm" href="{{ route('news.index') }}" content="{{ __('News') }}"/>
+    <x-cms.base.link class="!p-2 text-sm" href="{{ route('users.index') }}" content="{{ __('Users') }}"/>
 </div>
