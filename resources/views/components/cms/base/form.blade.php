8aa5c3e823a8fbe35f73d342c8a29b419ca0686b
Kenan Gündoğan <19374764+kenangundogan@users.noreply.github.com>
2024-06-26 15:01:46 +0300

    ```text refactor: Update form.blade.php to include enctype attribute
    The form.blade.php file has been updated to include the enctype attribute in the form tag. This change allows for proper handling of file uploads in the form.

--- resources/views/components/cms/base/form.blade.php
+++ resources/views/components/cms/base/form.blade.php
@@ -4,9 +4,10 @@
     $method = $method ?? '';
     $action = $action ?? '';
     $class = $class ?? '';
+    $enctype = $enctype ?? '';
 @endphp
 
-<form method="{{ $method }}" action="{{ $action }}" id="{{ $id }}" name="{{ $name }}" class="{{ $class }}">
+<form method="{{ $method }}" action="{{ $action }}" id="{{ $id }}" name="{{ $name }}" class="{{ $class }}" enctype="{{ $enctype }}">
     @csrf
     {{ $slot }}
 </form>
