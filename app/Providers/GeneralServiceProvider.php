d6b39b5338fa5d51120021ecc0deff4fd4f21add
Kenan Gündoğan <19374764+kenangundogan@users.noreply.github.com>
2024-06-27 22:32:23 +0300

    refactor: Update database configuration to use MySQL instead of SQLite
    The database configuration has been updated to use MySQL instead of SQLite. This change ensures that the application is using the appropriate database driver for production use. The `.env.example` file and the `config/database.php` file have been modified to reflect this change.

--- app/Providers/GeneralServiceProvider.php
+++ app/Providers/GeneralServiceProvider.php
@@ -22,18 +22,22 @@ class GeneralServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (!Schema::hasTable('categories')) {
+        try {
+            if (!Schema::hasTable('categories')) {
+                return;
+            }
+
+            if (Category::all()->isEmpty()) {
+                return;
+            }
+
+            $categories = Category::all();
+            $news = News::all();
+            $categories = $categories->whereIn('id', $news->pluck('category_id'));
+
+            view()->share('categories', $categories);
+        } catch (\Exception $e) {
             return;
         }
-
-        if (Category::all()->isEmpty()) {
-            return;
-        }
-
-        $categories = Category::all();
-        $news = News::all();
-        $categories = $categories->whereIn('id', $news->pluck('category_id'));
-
-        view()->share('categories', $categories);
     }
 }
