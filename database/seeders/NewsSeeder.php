78aa0feabfb348cb4a5e0b26b7545a8e456c80c4
Kenan Gundogan <kenangundogann@gmail.com>
2024-08-09 22:16:08 +0300

    refactor: Remove unused news paragraphs in NewsSeeder
    This commit removes the unused code that generated paragraphs of text in the NewsSeeder.php file. The paragraphs were not being used and were unnecessary for the seeding process. This refactor improves the efficiency and readability of the code.

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -16,17 +16,6 @@ class NewsSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        $paragraphs = '<p class="block mb-4">' . $faker->text(600) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(400) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(300) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(600) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(200) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(100) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(400) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(600) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(400) . '</p>';
-        $paragraphs .= '<p class="block mb-4">' . $faker->text(300) . '</p>';
-
         $categoryList = [
             'history',
             'children',
@@ -51,11 +40,10 @@ class NewsSeeder extends Seeder
             $categoryMap[$category] = $key + 1;
         }
 
-        // Kategorilere göre imaj ID'lerini ayıralım
         $categoryImages = [];
         foreach ($categoryList as $key => $category) {
             $categoryImages[$category] = range($key * 6 + 1, ($key + 1) * 6);
-            shuffle($categoryImages[$category]); // Rastgele sıraya diz
+            shuffle($categoryImages[$category]);
         }
 
         $news_content_types = [
@@ -69,19 +57,17 @@ class NewsSeeder extends Seeder
         ];
 
 
-        $totalNews = 90; // Toplamda 90 haber eklemeyi hedefliyoruz
+        $totalNews = 90;
         $insertedNewsCount = 0;
 
         while ($insertedNewsCount < $totalNews) {
             $randomCategoryKey = array_rand($categoryList);
             $randomCategory = $categoryList[$randomCategoryKey];
 
-            // Eğer kategori için kullanılabilir imaj kalmadıysa, başka kategori seçmeye devam et
             if (empty($categoryImages[$randomCategory])) {
                 continue;
             }
 
-            // Kullanılacak imaj ID'sini alalım ve listeden kaldıralım
             $imageID = array_pop($categoryImages[$randomCategory]);
 
             DB::table('news')->insert([
